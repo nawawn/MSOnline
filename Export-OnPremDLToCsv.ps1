3a2715dba7ffca90ddecaf465871bebec0a89ea0
Naw Awn <nawawn@googlemail.com>
2019-04-03 10:32:00 +0100

    Added Export On-Prem Distribution List to Csv script

---
+++ Export-OnPremDLToCsv.ps1
@@ -0,0 +1,35 @@
+[CmdletBinding()]
+Param()
+
+$AllDG = Get-DistributionGroup | Sort-Object name
+
+function Resolve-ADCanonicalName{
+    [CmdletBinding()]    
+    [OutputType([String])]
+    Param( 
+        [Parameter(Mandatory,ValueFromPipeline,ValueFromPipelineByPropertyName,Position=0)]
+        $CanonicalName 
+    )
+    Begin{}
+    Process{
+        return ((Get-ADUser -Filter 'Enabled -eq $true' -Properties CanonicalName).Where{$_.CanonicalName -eq $CanonicalName} | 
+            Select-Object -ExpandProperty Name
+        )
+    }
+}
+
+$CsvDG = Foreach($DG in $AllDG){
+        Write-Verbose "$DG"
+        [PSCustomObject]@{
+            Name    = $DG.Name
+            Alias   = $DG.Alias        
+            Owner   = $DG.Owner        
+            Members = ((Get-DistributionGroupMember -Identity $DG.Name).Name) -Join ","
+            Type    = If ($DG.RecipientType -eq 'MailUniversalDistributionGroup') {"Distribution"} Else {"Security"}
+            ManagedBy   = If ($DG.ManagedBy){ ($DG.ManagedBy | Resolve-ADCanonicalName) -join "," } Else { $null }
+            DisplayName = $DG.DisplayName
+            PrimarySMTPAddress = $DG.PrimarySMTPAddress
+        }
+    }
+
+$CsvDG | Export-csv  C:\Temp\AllDistList.csv -NoTypeInformation  
