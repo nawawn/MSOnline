519f1fa6555aa3307fd2c6bcc63e0156872762fa
nawawn <nawawn@googlemail.com>
2019-04-07 21:04:04 +0100

    Added Import DG to O365 script

---
+++ Import-DistributionGroup.ps1
@@ -0,0 +1,57 @@
+﻿[CmdletBinding()]
+Param(
+    $Path = "C:\Temp\AllDistList.csv"
+)
+
+If (!(Test-Path -Path $Path -PathType leaf)){
+    Write-Warning "The CSV file path is not found!"
+    return
+}
+
+If(!((Get-PSSession).where{$_.ConfigurationName -eq "Microsoft.Exchange"})){
+    Write-Warning "Please Connect to Exchange Online PowerShell!"
+    return
+}
+
+Function Test-EODistributionGroup{
+    [OutputType([Bool])]
+    Param( $Name )
+    $AlreadyExist = Get-DistributionGroup -Identity $Name -ErrorAction SilentlyContinue
+    return ($null -ne $AlreadyExist)
+<#
+.EXAMPLE
+   Test-EODistributionGroup -Name "DG-Test"
+#> 
+}
+
+#region Controller
+
+$AllDG = Import-Csv -Path $Path
+
+Foreach($DG in $AllDG){   
+    $Identity = $DG.Name
+    $Members  = If($($DG.Members)){$DG.Members -split ","} Else{ $null }
+       
+    If (!(Test-EODistributionGroup -Name $Identity)){        
+        New-DistributionGroup -Name $Identity -Alias $($DG.Alias) -DisplayName $($DG.DisplayName) -Type $($DG.Type) -PrimarySMTPAddress $($DG.PrimarySMTPAddress)
+        Write-Verbose "$Identity is created."
+        Start-Sleep 3
+        If($Members){
+            Foreach ($m in $Members){            
+                Add-DistributionGroupMember -Identity $Identity -Member $m -Confirm:$false
+                Write-Verbose " - $m has been added to $Identity!"
+            }
+        }
+    }
+    Else {
+        Write-Warning "$Identity already exists!"
+        $CurrentMem = (Get-DistributionGroupMember -Identity $Identity).Name
+        If(($null -eq $CurrentMem) -and ($Members)){
+            Foreach ($m in $Members){            
+                Add-DistributionGroupMember -Identity $Identity -Member $m -Confirm:$false
+                Write-Verbose " - $m has been added to $Identity!"
+            }
+        }
+    }
+}
+#endregion Controller
