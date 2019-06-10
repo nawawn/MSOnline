0e1e715cd92762d7e69d2b988fe91810d6e49888
Naw Awn <nawawn@googlemail.com>
2019-06-10 17:51:15 +0100

    Added Set-MFAo365.ps1 and updated ReadMe file.

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ __Import-BulkUsers.ps1__ This script imports bulk users to office 365 environmen
 __Export-OnPremDLToCsv.ps1__ This script exports all the distribution list from on-prem exchange server to a csv file.
 
 __Import-DistributionGroup.ps1__ This script imports distribution group from a csv file to office 365 Exchange Online.
+
+__Set-MFAo365.ps1__ This script has two functions: Enable-MFAO365 and Disable-MFAO365 that allow you to either enable or disable multifactor authentication on office 365 user.
---
+++ Set-MFAo365.ps1
@@ -0,0 +1,55 @@
+﻿Function Enable-MFAO365{    
+    Param(
+        [Parameter(Mandatory,ValueFromPipeline,ValueFromPipelineByPropertyName,Position=0)]
+        [String]$UserPrincipalName
+    )
+    Begin{
+        $MFA = New-Object -TypeName Microsoft.Online.Administration.StrongAuthenticationRequirement        
+        $MFA.State        = "Enabled"
+        $MFA.RelyingParty = "*"
+        $MFA.RememberDevicesNotIssuedBefore = (Get-Date)
+    }
+    Process{
+        $MFAState = (Get-MsolUser -UserPrincipalName $UserPrincipalName).StrongAuthenticationRequirements
+        If ($MFAState){
+            Write-Output "$UserPrincipalName - MFA already $($MFAState.State)"
+        }
+        Else {
+            Write-Verbose "$UserPrincipalName - MFA is getting enabled"
+            Set-MsolUser -UserPrincipalName $UserPrincipalName -StrongAuthenticationRequirements $MFA
+        } 
+    }
+<#
+.EXAMPLE
+   Enable-MFAO365 -UserPrincipalName <UserPrincipalName>
+.EXAMPLE
+   Get-MsolUser -UserPrincipalName <UserPrincipalName> | Enable-MFAO365
+#>
+}
+
+Function Disable-MFAO365{    
+    Param(
+        [Parameter(Mandatory,ValueFromPipeline,ValueFromPipelineByPropertyName,Position=0)]
+        [String]$UserPrincipalName
+    )
+    Begin{
+        $MFA = @()
+    }
+    Process{
+        $MFAState = (Get-MsolUser -UserPrincipalName $UserPrincipalName -ErrorAction SilentlyContinue).StrongAuthenticationRequirements
+        If ($MFAState){
+            Write-Verbose "$UserPrincipalName - MFA is getting disabled"
+            Set-MsolUser -UserPrincipalName $UserPrincipalName -StrongAuthenticationRequirements $MFA
+            
+        }
+        Else {
+            Write-Output "$UserPrincipalName - MFA is already disabled"
+        } 
+    }
+<#
+.EXAMPLE
+   Disable-MFAO365 -UserPrincipalName <UserPrincipalName>
+.EXAMPLE
+   Get-MsolUser -UserPrincipalName <UserPrincipalName> | Disable-MFAO365
+#>
+}
